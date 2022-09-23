fad4604c240888ab0d82483b9e2fe1dd6c23baa5
Kate Orlova <kate.s.orlova@gmail.com>
2022-09-23 22:54:57 +0100

    ConfigSettings class

---
+++ src/Sitecore/SitecoreCDP/Foundation/SitecoreCDP/Configuration/ConfigSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace SitecoreCDP.Configuration
+{
+    public static class ConfigSettings
+    {
+        public static string GetSetting(string settingName, string defaultValue = null)
+        {
+            var settingValue = Sitecore.Configuration.Settings.GetSetting(settingName);
+
+            if (string.IsNullOrEmpty(settingValue))
+            {
+                settingValue = defaultValue;
+            }
+            return settingValue;
+        }
+    }
+}
