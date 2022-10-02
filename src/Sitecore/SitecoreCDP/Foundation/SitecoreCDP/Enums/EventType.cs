f9564722c4225e4a35792cba315075e267895996
Kate Orlova <kate.s.orlova@gmail.com>
2022-10-02 23:18:37 +0100

    Created enum for reserved event types in CDP

---
+++ src/Sitecore/SitecoreCDP/Foundation/SitecoreCDP/Enums/EventType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Foundation.SitecoreCDP.Enums
+{
+    public enum EventType
+    {
+        IDENTITY,
+        LOGIN,
+        VIEW
+
+    }
+}
