a76af13e240aa9a11a471385924ee0f064981dc2
Anna Gevel <anngevel@gmail.com>
2022-12-13 13:19:48 +0000

    installed Newtonsoft.Json

--- src/Sitecore/SitecoreCDP/Feature/FullStackExperience/Models/FlowExecutionResult.cs
+++ src/Sitecore/SitecoreCDP/Feature/FullStackExperience/Models/FlowExecutionResult.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace FullStackExperience.Models
 {
     public class FlowExecutionResult
     {
+        [JsonProperty("guestEmail")]
         public string GuestEmail { get; set; }
 
+        [JsonProperty("guestName")]
         public string GuestName { get; set; }
 
+        [JsonProperty("contentItemId")]
         public Guid ContentItemId { get; set; }
     }
 }
