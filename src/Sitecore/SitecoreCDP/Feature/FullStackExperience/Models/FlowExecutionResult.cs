3e204025a0d4aff45546083dcbec52e90108cc55
Anna Gevel <anngevel@gmail.com>
2022-12-13 13:05:38 +0000

    Feature project for full stack experience integration

---
+++ src/Sitecore/SitecoreCDP/Feature/FullStackExperience/Models/FlowExecutionResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FullStackExperience.Models
+{
+    public class FlowExecutionResult
+    {
+        public string GuestEmail { get; set; }
+
+        public string GuestName { get; set; }
+
+        public Guid ContentItemId { get; set; }
+    }
+}
---
+++ src/Sitecore/SitecoreCDP/Feature/FullStackExperience/Services/FlowExecutionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FullStackExperience.Models;
+
+namespace FullStackExperience.Services
+{
+    public interface IFlowExecutionService
+    {
+        FlowExecutionResult ExecuteFlow();
+    }
+
+    public class FlowExecutionService : IFlowExecutionService
+    {
+        public FlowExecutionResult ExecuteFlow()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
