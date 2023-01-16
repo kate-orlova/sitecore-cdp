d6eccca406ea8c3b7c34210790a8a1bf992087c5
Anna Gevel <anngevel@gmail.com>
2023-01-16 17:03:13 +0000

    added two new models for banner offer response

---
+++ src/Sitecore/SitecoreCDP/Feature/FullStackExperience/Models/BannerOfferTemplate.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace FullStackExperience.Models
+{
+    public class BannerOfferTemplate
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("imageUrl")]
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ src/Sitecore/SitecoreCDP/Feature/FullStackExperience/Models/FlowExecutionOfferResult.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace FullStackExperience.Models
+{
+    public class FlowExecutionOfferResult
+    {
+        [JsonProperty("guestEmail")]
+        public string GuestEmail { get; set; }
+
+        [JsonProperty("guestName")]
+        public string GuestName { get; set; }
+
+        [JsonProperty("offer")]
+        public BannerOfferTemplate Offer { get; set; }
+    }
+}
