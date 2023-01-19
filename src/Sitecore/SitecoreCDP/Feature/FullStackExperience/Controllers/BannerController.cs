3dd5e82ecbb86f4b750c1baf3b1dee744667af0d
Anna Gevel <anngevel@gmail.com>
2023-01-19 15:07:50 +0000

    component for the offer banner

--- src/Sitecore/SitecoreCDP/Feature/FullStackExperience/Controllers/BannerController.cs
+++ src/Sitecore/SitecoreCDP/Feature/FullStackExperience/Controllers/BannerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web.Mvc;
 using Foundation.SitecoreCDP.Configuration;
 using FullStackExperience.Models;
+using FullStackExperience.Models.Api;
 using FullStackExperience.Services;
 using Sitecore.Data;
 
@@ -42,6 +43,32 @@ namespace FullStackExperience.Controllers
             }
 
             return View("~/Views/FullStackExperience/Banner.cshtml", model);
+        }
+        public ActionResult OfferBanner()
+        {
+
+            var request = new FlowExecutionRequest()
+            {
+                Channel = ConfigSettings.Channel,
+                ClientKey = ConfigSettings.ClientKey,
+                CurrencyCode = ConfigSettings.Currency,
+                Email = Sitecore.Context.User.Profile?.Email,
+                FriendlyId = ConfigSettings.OfferExperienceId,
+                Language = Sitecore.Context.Item.Language.Name,
+                PointOfSale = ConfigSettings.POS
+            };
+
+            var result = _flowExecutionService.ExecuteFlow<FlowExecutionOfferResult>(request).GetAwaiter().GetResult();
+            var model = new OfferBannerModel()
+            {
+                GuestEmail = result?.GuestEmail,
+                GuestName = result?.GuestName,
+                Title = result?.Offer?.Title,
+                Description = result?.Offer?.Description,
+                ImageUrl = result?.Offer?.ImageUrl
+            };
+
+            return View("~/Views/FullStackExperience/OfferBanner.cshtml", model);
         }
     }
 }
---
+++ src/Sitecore/SitecoreCDP/Feature/FullStackExperience/Models/OfferBannerModel.cs
@@ -0,0 +1,15 @@
+﻿namespace FullStackExperience.Models
+{
+    public class OfferBannerModel
+    {
+        public string GuestEmail { get; set; }
+
+        public string GuestName { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
