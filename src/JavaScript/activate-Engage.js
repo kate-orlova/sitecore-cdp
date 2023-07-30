252dd768db33c0d2b6e9a434ffeba2206b4b5e65
Kate Orlova <kate.s.orlova@gmail.com>
2023-07-30 22:33:40 +0100

    Activate script with Engage JS library

---
+++ src/JavaScript/activate-Engage.js
@@ -0,0 +1,39 @@
+<script>
+    // Initialize the engage variable 
+    var engage = undefined;
+
+    // Create and inject the <script> tag into the HTML 
+    var s = document.createElement("script"); 
+    s.type = "text/javascript"; 
+    s.async = true; 
+    s.src = "https://d1mj578wat5n4o.cloudfront.net/sitecore-engage-v.1.1.0.min.js"; 
+    var x = document.querySelector("script"); 
+    x.parentNode.insertBefore(s, x); 
+
+    // Initialize the Engage JavaScript Library 
+    s.addEventListener("load", function () { 
+      var settings = { 
+        clientKey: "<client_key_PLACEHOLDER>", 
+        targetURL: "<stream_api_target_endpoint_PLACEHOLDER>", 
+        pointOfSale: "<point_of_sale_PLACEHOLDER>", 
+        cookieDomain: "<cookie_domain_PLACEHOLDER>", 
+        cookieExpiryDays: 365, 
+        forceServerCookieMode: false, 
+        includeUTMParameters: true, 
+        webPersonalization: "<boolean_or_object>" 
+    }; 
+
+    window.Engage.init(settings).then(function (result) { 
+      engage = result; 
+
+      // Send a VIEW event 
+      var event = { 
+        channel: "<channel_PLACEHOLDER>", 
+        language: "<language_PLACEHOLDER>", 
+        currency: "<currency_PLACEHOLDER>", 
+        page: "<page_PLACEHOLDER>" 
+      }; 
+     engage.pageView(event); 
+    }); 
+  }); 
+</script>  
